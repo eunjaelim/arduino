172b6ae53de004b4378f0c3b81dd5cc0a623821e
eunjaelim <imeunjae361@gmail.com>
2023-03-30 12:25:22 +0900

    Z작전

---
+++ Eight_LED/Eight_LED.ino
@@ -0,0 +1,93 @@
+void setup() {
+
+  // put your setup code here, to run once:
+
+  pinMode(4, OUTPUT);
+
+  pinMode(5, OUTPUT);
+
+  pinMode(6, OUTPUT);
+
+  pinMode(7, OUTPUT);
+
+  pinMode(8, OUTPUT);
+
+  pinMode(9, OUTPUT);
+
+  pinMode(10, OUTPUT);
+
+  pinMode(11, OUTPUT);
+
+}
+
+
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+
+  digitalWrite(4, HIGH);
+
+  delay(50);
+
+  digitalWrite(5, HIGH);
+
+  delay(50);
+
+  digitalWrite(6, HIGH);
+
+  delay(50);
+
+  digitalWrite(7, HIGH);
+
+  delay(50);
+
+  digitalWrite(8, HIGH);
+
+  delay(50);
+
+  digitalWrite(9, HIGH);
+
+  delay(50);
+
+  digitalWrite(10, HIGH);
+
+  delay(50);
+
+  digitalWrite(11, HIGH);
+
+  delay(50);
+
+  digitalWrite(11, LOW);
+
+  delay(50);
+
+  digitalWrite(10, LOW);
+
+  delay(50);
+
+  digitalWrite(9, LOW);
+
+  delay(50);
+
+  digitalWrite(8, LOW);
+
+  delay(50);
+
+  digitalWrite(7, LOW);
+
+  delay(50);
+
+  digitalWrite(6, LOW);
+
+  delay(50);
+
+  digitalWrite(5, LOW);
+
+  delay(50);
+
+  digitalWrite(4, LOW);
+
+  delay(50);
+
+}
